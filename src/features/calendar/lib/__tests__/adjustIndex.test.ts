20990c32bccb526444f81e898f9cbe6d7773c841
chipmunkcol <jack95711@gmail.com>
2025-06-06 18:07:25 +0900

    fix: adjustIndex 테스트 코드 수정

--- src/features/calendar/lib/__tests__/adjustIndex.test.ts
+++ src/features/calendar/lib/__tests__/adjustIndex.test.ts
@@ -14,7 +14,7 @@ describe('getAdjustedIndex', () => {
     expect(getAdjustedIndex(4, [0, 1, 2, 4])).toBe(4);
   });
 
-  it('lower indices가 없으면 lower indices 중 첫번째 index로 재할당', () => {
+  it('lower indices가 있으면 lower indices 중 첫번째 index로 재할당', () => {
     expect(getAdjustedIndex(3, [0, 3])).toBe(1);
     expect(getAdjustedIndex(3, [0, 1, 3])).toBe(2);
   });
@@ -22,16 +22,5 @@ describe('getAdjustedIndex', () => {
   it('높은 index가 여러개 있으면 순서대로 재할당', () => {
     expect(getAdjustedIndex(3, [3, 4])).toBe(0);
     expect(getAdjustedIndex(4, [3, 4])).toBe(1);
-  });
-
-  it('lower indices가 일부 사용되면 lower indices 중 첫번째 index로 재할당', () => {
-    expect(getAdjustedIndex(3, [1, 3])).toBe(0);
-    expect(getAdjustedIndex(3, [0, 3])).toBe(1);
-  });
-
-  it('높은 index가 여러개 있으면 순서대로 재할당', () => {
-    const allIndices = [0, 3, 4, 5];
-    expect(getAdjustedIndex(3, allIndices)).toBe(1);
-    expect(getAdjustedIndex(4, allIndices)).toBe(2);
   });
 });
