b550c479a50cf973734e7a2cfb67968b317aa4ae
chipmunkcol <jack95711@gmail.com>
2025-06-22 22:04:41 +0900

    fix: calendar가 매번 현재 달을 표시하는 버그 & 비로그인 시 예시데이터 key value 추가 (location : String)

--- src/entities/calendar/consts/constants.ts
+++ src/entities/calendar/consts/constants.ts
@@ -1,3 +1,5 @@
+import { ColorKey } from '../api/types'
+
 export const DAY_OF_THE_WEEK = ['일', '월', '화', '수', '목', '금', '토']
 
 export const COLORS = {
@@ -14,16 +16,16 @@ export const COLORS = {
 } as const
 
 const rawExampleSchedules = [
-  { title: '제주도 여행', startDay: 2, endDay: 5, color: 'ORANGE' },
-  { title: '지연이랑 성수동', startDay: 15, endDay: 15, color: 'TEAL' },
-  { title: '가족 식사', startDay: 20, endDay: 20, color: 'LIGHT_PURPLE' },
-  { title: '자격증 시험', startDay: 27, endDay: 27, color: 'BLUE' },
+  { title: '제주도 여행', startDay: 2, endDay: 5, color: 'ORANGE', location: '제주도' },
+  { title: '지연이랑 성수동', startDay: 15, endDay: 15, color: 'TEAL', location: '성수 카페거리' },
+  { title: '가족 식사', startDay: 20, endDay: 20, color: 'LIGHT_PURPLE', location: '남양주' },
+  { title: '자격증 시험', startDay: 27, endDay: 27, color: 'BLUE', location: '국세청' },
 ]
 
 function generateEcampleGuideSchedules() {
   const now = new Date()
 
-  return rawExampleSchedules.map(({ title, startDay, endDay, color }, idx) => {
+  return rawExampleSchedules.map(({ title, startDay, endDay, color, location }, idx) => {
     const startDate = new Date(now.getFullYear(), now.getMonth(), startDay)
     const endDate = new Date(now.getFullYear(), now.getMonth(), endDay)
 
@@ -36,7 +38,8 @@ function generateEcampleGuideSchedules() {
       content: title,
       startDateTime: startDate.toISOString(),
       endDateTime: endDate.toISOString(),
-      color,
+      color: color as ColorKey,
+      location,
     }
   })
 }
