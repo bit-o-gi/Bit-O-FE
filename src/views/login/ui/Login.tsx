ef922a37fda63ce77c5f59ae7c2ac11d2184cd26
BoeunHan <h.boeunn@gmail.com>
2025-07-01 22:45:18 +0900

    move: 로그인 페이지 이동 (app -> views)

---
+++ src/views/login/ui/Login.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Oauth from '@/features/oauth'
+import Image from 'next/image'
+import { Suspense } from 'react'
+
+export function LoginPage() {
+  return (
+    <div className="h-full">
+      <div className="h-full flex flex-col justify-center items-center gap-10 ">
+        <div>
+          <Image width={126} height={44} src={'/images/logo/BitO.svg'} alt="logo" />
+        </div>
+        <div className="text-center text-[21px]">
+          <div>간편하게 로그인하고</div>
+          <div>다양한 서비스를 이용해보세요.</div>
+        </div>
+        <Suspense>
+          <Oauth />
+        </Suspense>
+      </div>
+    </div>
+  )
+}
